30f1910b3fe5b5fa7f0237dd1c110ab4b561b199
Eduard <e9047689558@yandex.ru>
2022-07-27 12:00:57 +0300

    add interface

---
+++ src/com/company/MyArrayList.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public interface MyArrayList<T>
+{
+    boolean add(T t);
+    boolean add(int index);
+    T get(int index);
+    T get(T t);
+    void set(int index, T t);
+    void remove(int index);
+    void remove(T t);
+    int size();
+    int sort();
+}
